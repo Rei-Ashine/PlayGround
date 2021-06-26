86b2a4e39ba4ba9293afd75d02f6bc5d6a3c3982
Rei-Ashine <ashine.rei.74z@st.kyoto-u.ac.jp>
2021-06-26 14:59:26 +0900

    Add script

---
+++ scripts/record_increase-decrease.py
@@ -0,0 +1,20 @@
+def check_records(txt):
+    _ = txt.pop(0)
+    eve = txt.pop(0)
+    for record in txt:
+        the_day = record
+        if the_day == eve:
+            print(f"stay")
+        elif the_day < eve:
+            print(f"down {eve - the_day}")
+        elif the_day > eve:
+            print(f"up {the_day - eve}")
+        eve = the_day
+
+
+if __name__ == "__main__":
+    import sys
+    inputs = []
+    for line in sys.stdin:
+        inputs.append(int(line))
+    check_records(inputs)
